32f1efbcc509f05741586e6d57ad67c89db26838
iemkitt2 <103313269+iemkitt2@users.noreply.github.com>
2023-05-05 10:39:17 +0530

    Multiplication added

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator
 {
     public static void main(String [] args)
     {
-        System.out.println("Operation to be done : Add / Subtract");
+        System.out.println("Operation to be done : Add / Subtract / Multiply");
         Scanner s = new Scanner(System.in);
         String operation = s.next();
         if(operation.equalsIgnoreCase("Add"))
@@ -22,6 +22,14 @@ public class Calculator
             int n1 = s.nextInt();
             int n2 = s.nextInt();
             System.out.println("Difference = "+ (n1-n2));
+        }
+
+        else if(operation.equalsIgnoreCase("Multiply"))
+        {
+            System.out.println("Enter 2 numbers:");
+            int n1 = s.nextInt();
+            int n2 = s.nextInt();
+            System.out.println("Product = "+ (n1*n2));
         }
 
         else
