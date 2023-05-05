041c2f77201330acdcaff2749784441aeed771f9
iemkitt2 <103313269+iemkitt2@users.noreply.github.com>
2023-05-05 10:31:20 +0530

    Initial version

---
+++ src/Calculator.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+
+public class Calculator
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Operation to be done : Add / Subtract");
+        Scanner s = new Scanner(System.in);
+        String operation = s.next();
+        if(operation.equalsIgnoreCase("Add"))
+        {
+            System.out.println("Enter 2 numbers:");
+            int n1 = s.nextInt();
+            int n2 = s.nextInt();
+            System.out.println("Sum = "+ (n1+n2));
+        }
+
+        else if(operation.equalsIgnoreCase("Subtract"))
+        {
+            System.out.println("Enter 2 numbers:");
+            int n1 = s.nextInt();
+            int n2 = s.nextInt();
+            System.out.println("Difference = "+ (n1-n2));
+        }
+
+        else
+        {
+            System.out.println("Invalid choice");
+        }
+    }
+
+}
